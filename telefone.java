cf56e7317ad9233333934fa34f529c3db7fc0890
HTC-Gato <heitort3ixeira@gmail.com>
2025-08-28 10:06:17 -0300

    Create telefone.java

---
+++ telefone.java
@@ -0,0 +1,20 @@
+package exercicio3;
+
+importjava.util.Scanner
+
+public class Telefone<integer> {
+    public integer codigoArea;
+
+	public Telefone(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Telefone lerTelefone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+    
+
+	}
+
+
